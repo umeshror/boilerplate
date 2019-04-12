b67e834259c543b186b29a6fe5c54a3c9346212d
Umesh <umesh@kisanhub.com>
2019-04-12 15:21:35 +0530

    group similar array

---
+++ group_similar_array.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20];
+
+
+arr.sort();
+
+new_arr = [];
+
+arr.reduce(function (r, current_item) {
+    if (current_item !== r) {
+        new_arr.push([]);
+    }
+    new_arr[new_arr.length - 1].push(current_item);
+    return current_item;
+}, undefined);)
+
+
+// Output: [[1,1,1,1],[2,2],[3,3,3],4,[5,5],6]
+
+
