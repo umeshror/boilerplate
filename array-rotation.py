a74e30871d7f66cc1d475eeeba43205bc7f484f8
umeshror <sarukumesh@gmail.com>
2019-05-16 21:56:19 +0530

    Create array-rotation.py

---
+++ array-rotation.py
@@ -0,0 +1,16 @@
+def right_rotation(a, k):
+   rotations = k % len(a)
+   return a[-rotations:] + a[:-rotations]
+
+def left_rotation(a, k):
+   rotations = k % len(a)
+   return a[rotations:] + a[:rotations]
+   
+arr = [1,2,3,4,5]
+right_rotation(arr, 2)
+# output: [4, 5, 1, 2, 3]
+
+
+right_rotation(arr, 2)
+# output: [3, 4, 5, 1, 2]
+
